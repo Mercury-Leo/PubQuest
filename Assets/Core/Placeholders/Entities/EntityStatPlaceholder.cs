b94c8a13cb8d44ad442df925036ab7443206b147
Mercury <>
2022-08-29 13:25:08 +0300

    Removed

--- Assets/Core/Placeholders/Entities/EntityStatPlaceholder.cs
+++ Assets/Core/Placeholders/Entities/EntityStatPlaceholder.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Core.Placeholders.Entities {
     [Serializable]
-    public class EntityStatPlaceholder : IEquatable<EntityStatPlaceholder> {
+    public class EntityStatPlaceholder {
         #region Properties
 
         public float Health { get; set; }
@@ -22,22 +22,5 @@ namespace Core.Placeholders.Entities {
         }
 
         #endregion
-
-        public bool Equals(EntityStatPlaceholder other) {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return Health.Equals(other.Health) && Speed.Equals(other.Speed) && Defense.Equals(other.Defense);
-        }
-
-        public override bool Equals(object obj) {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((EntityStatPlaceholder)obj);
-        }
-
-        public override int GetHashCode() {
-            return HashCode.Combine(Health, Speed, Defense);
-        }
     }
 }
